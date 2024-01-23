e708e7276df31918d6f554415fcd1f29aa98a2b8
alexandrbogdanov <al1029ex@gmail.com>
2024-01-23 16:08:06 +0100

    working version of a test finished

--- tests/test_demoqa_practice_form.py
+++ tests/test_demoqa_practice_form.py
@@ -5,11 +5,8 @@ from selene import browser, be, have, by, command
 
 def test_practice_form():
     browser.open('/automation-practice-form')
-    # remove()
     remove("#fixedban")
     remove("footer")
-    browser.execute_script('document.querySelector("#fixedban").remove()')
-    browser.execute_script('document.querySelector("footer").remove()')
     browser.element('#firstName').should(be.blank).type('Benedict')
     browser.element('#lastName').should(be.blank).type('Cumberbatch')
     browser.element('#userEmail').should(be.blank).type('benya@mail.ru')
@@ -26,15 +23,34 @@ def test_practice_form():
     browser.element(".react-datepicker__day--024").click()
     browser.element("#subjectsInput").should(be.blank).click().type('Maths').press_enter()
     browser.element("[for='hobbies-checkbox-2']").click()
+    scroll_to("#submit")
     browser.element("#uploadPicture").send_keys(
         path.abspath("resources/bkb.jpeg"))
-    # browser.element("#currentAddress").click()
-
-    # browser.execute_script()
-    # react-datepicker__month-container
-    browser.open('/automation-practice-form')
+    browser.element("#currentAddress").should(be.blank).type("221b, Baker Str.")
+    browser.element("#state").click()
+    browser.element(by.text('NCR')).click()
+    browser.element("#city").click()
+    browser.element(by.text('Delhi')).click()
+    browser.element("#submit").click()
+    browser.element(".modal-dialog").should(be.existing)
+    browser.element(".table").all('td:nth-child(2)').should(have.exact_texts(
+        'Benedict Cumberbatch',
+        'benya@mail.ru',
+        'Male',
+        '0123456789',
+        '24 September,1977',
+        'Maths',
+        'Reading',
+        'bkb.jpeg',
+        '221b, Baker Str.',
+        'NCR Delhi',
+    ))
 
 
 def remove(element):
     js_code = f"document.querySelector(\"{element}\").remove();"
     browser.driver.execute_script(js_code)
+
+
+def scroll_to(element):
+    browser.element(element).perform(command.js.scroll_into_view)
